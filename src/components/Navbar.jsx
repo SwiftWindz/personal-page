08682907f5896d367ac736f68e78ee35c98781cc
Phil Ganem <philipganem@gmail.com>
2022-06-05 15:34:52 -0400

    Fixed mobile bar i think

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,12 +18,15 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className="flex md:hidden mt-[-75px]" onClick={handleClick}>
-                    {!nav ? <button><MenuIcon className="w-10 pt-5 animate-pulse text-zinc-700"/></button> : <button><XIcon className="text-zinc-700 pt-5 w-10 animate-pulse py-5"/></button>}
+                    {!nav ? <button><MenuIcon className="w-10 pt-5 animate-pulse text-zinc-700"/></button> : 
+                    <div className="w-screen sm:h-[50px] md:h-[80px] z-10 fixed drop-shadow-lg">
+                        <button><XIcon className="text-zinc-700 pt-5 w-10 animate-pulse py-5"/></button>
+                    </div>}
                 </div>
             </div>
             <div className="md:hidden font-outfit">
-                <ul className={!nav ? "hidden" : "absolute bg-white  w-full h-screen px-8 py-10"}>
-                <h2 className="py-1">PG</h2>
+                <ul className={!nav ? "hidden" : "absolute bg-white  w-full h-screen px-8 py-16"}>
+                <h2 className="py-1 font-bold">PG</h2>
                 <li className="hover:text-indigo-600 border-b-2 border-zinc-300 w-full text-2xl"><button onClick={handleClick}><Link to="/">About</Link></button></li>
                 <li className="hover:text-indigo-600 border-b-2 border-zinc-300 w-full text-2xl"><button onClick={handleClick}><Link to="/portfolio">Portfolio</Link></button></li>
                 <li className="hover:text-indigo-600 border-b-2 border-zinc-300 w-full text-2xl"><button onClick={handleClick}><Link to="/resume">Resume</Link></button></li>
