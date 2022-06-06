3925bd03baea0cdad6ddc7a1af4d660dcff9ca10
Phil Ganem <philipganem@gmail.com>
2022-06-06 02:31:20 -0400

    Resume page done

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import Footer from '../components/Footer'
-import pdf from '../assets/normal .pdf';
+import pdf from '../assets/GanemResume.pdf';
 
 
 export default function Resume() {
 
   return (
     <>
-      <div className='w-screen h-screen pt-[100px] font-outfit bg-zinc-600 text-white text-center'>
-        <div className='grid place-items-center '>
-          <embed className='w-[600px] h-[800px]' src={pdf} type="" />
+      <div className='w-full h-full pt-[100px] font-outfit bg-gradient-to-br from-violet-600 to-gray-400 text-white text-center'>
+        <div className='grid place-items-center pb-10'>
+          <div className='border m-4 p-3 md:m-0 md:p-10 rounded-xl shadow-2xl bg-white grid place-items-center pb-10'>
+            <div className='pb-10'>
+              <p className='text-6xl font-bold text-purple-500'>My Resume. Grab one!</p>
+            </div>
+            <embed className='w-[400px] h-[550px] md:w-[700px] md:h-[900px]' src={pdf} type="" />
+          </div>
         </div>
       </div>
       <Footer></Footer>
