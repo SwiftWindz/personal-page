3db7ec7cb75a09bc8dda083ac0163d24479ec3bd
Phil Ganem <philipganem@gmail.com>
2022-06-13 23:22:03 -0400

    Done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,18 +9,83 @@ export default function About() {
                 <p className='text-2xl md:text-3xl text-white py-6 pb-10'>My name is Phil Ganem and I'm a student software engineer, activist, and coffee snob based out of Rochester, New York. I'm currently 
                 I am currently working my way through the Rochester Institute of Technology's five year software engineering program.</p>
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-3 gap-4 px-6 md:px-2 text-center'>
-                <div className='border py-8 rounded-xl shadow-2xl bg-white'>
-                    <p className='text-5xl lg:text-6xl font-bold text-indigo-600'>Lorem.</p>
-                    <p className='text-black mt-2 px-5 py-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias neque magnam necessitatibus hic, qui odit nisi eligendi eaque obcaecati totam deleniti dolor at distinctio sequi quis, modi accusamus eius vero. Ea excepturi, placeat culpa nihil necessitatibus a in quo dolores suscipit quas similique maiores autem sit, voluptate iure, modi dolorem dolor porro ducimus optio ullam. Dolorum assumenda dolores ipsum dolorem cumque sed non pariatur explicabo, veritatis quo aut eligendi sit obcaecati reprehenderit? Dignissimos non velit, excepturi cupiditate consectetur veniam quod!</p>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-4 px-6 md:px-2 text-center '>
+                <div className='border py-8 rounded-xl shadow-2xl bg-white text-black'>
+                    <p className='text-5xl lg:text-6xl font-bold text-indigo-600'>Education</p>
+                    <div className='px-2'> 
+                        <div className='pt-2 pb-2 border-b-2 border-b-stone-700'>
+                            <h2 className='font-bold'>Rochester Institute of Technology</h2>
+                            <h3>Bachelor of Science Software Engineering 2025</h3>
+                            <h3>GPA: 3.69 / 4.00 </h3>
+                        </div>
+                        <div className='pt-2'>
+                            <p>Noteworthy Courses: Engineering of Software Subsystems (<strong>SWEN262</strong>), 
+                            Intro to Software Engineering (<strong>SWEN261</strong>), 
+                            Personal Software Engineering (<strong>SWEN250</strong>), 
+                            Web Engineering (<strong>SWEN344</strong>), 
+                            Software Process and Project Managment (<strong>SWEN256</strong>), 
+                            Software Developement and Problem Solving I (<strong>SWEN123</strong>), 
+                            Software Developement and Problem Solving II (<strong>SWEN124</strong>), 
+                            Software Engineering Freshman Seminar (<strong>SWEN101</strong>).
+                            </p>
+                        </div>
+                    </div>                 
+
+                {/* <p className='text-black pt-2 p-2'>
+                        As you might have seen, I am a student at the Rochester Institute of Technology. I am currently in my third year of their five year software engineering program. I am expected to graduate 
+                        in May 2025.
+                    </p>
+                    <p className='text-black pt-2 p-2'>Courses of note:
+                    Noteworthy Courses: Engineering of Software Subsystems (<strong>SWEN262</strong>), 
+                Intro to Software Engineering (<strong>SWEN261</strong>), 
+                Personal Software Engineering (<strong>SWEN250</strong>), 
+                Web Engineering (<strong>SWEN344</strong>), 
+                Software Process and Project Managment (<strong>SWEN256</strong>), 
+                Software Developement and Problem Solving I (<strong>SWEN123</strong>), 
+                Software Developement and Problem Solving II (<strong>SWEN124</strong>), 
+                Software Engineering Freshman Seminar (<strong>SWEN101</strong>).</p> */}
                 </div>
                 <div className='border py-8 rounded-xl shadow-2xl bg-white'>
-                    <p className='text-5xl lg:text-6xl font-bold text-indigo-600'>Lorem.</p>
-                    <p className='text-black mt-2 px-2 py-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae sunt molestiae sed itaque nemo sint architecto laboriosam dolore exercitationem consectetur, ab magnam perferendis accusamus unde beatae in error est amet porro saepe quod nam tempora suscipit dolor. Nam temporibus consectetur similique voluptatem distinctio provident alias exercitationem hic molestias ut, ipsum laborum architecto fuga cumque eum id! Dignissimos et minima similique atque facilis reiciendis. Beatae aliquam, neque, adipisci aperiam necessitatibus saepe provident consequuntur velit quibusdam quo rem fugit qui iusto quos!</p>
+                    <p className='text-5xl lg:text-6xl font-bold text-indigo-600'>Experience</p>
+                    <div className='px-2 text-black'> 
+                        <div className='pt-2 pb-2 border-b-2 border-b-stone-700'>
+                            <h2 className='font-bold'>Rochester Institute of Technology</h2>
+                            <h3>Software Engineering Course Assistant</h3>
+                            <h3>Aug 2021 to Present</h3>
+                        </div>
+                        <div className='pt-2'>
+                            <ul className='list-disc list-inside'>
+                                <li>
+                                Grade assignments and exams in Python and Java for Rochester Institute of Technology's Software
+                                Engineering and Problem Solving 1 & 2 course sequence.
+                                </li>
+                                <li>
+                                Work one-on-one with students during and after classtime to guide students to an understanding of course content.
+                                </li>
+                            </ul>
+                         
+                        </div>
+                    </div>     
+                    
                 </div>
-                <div className='border py-8 rounded-xl shadow-2xl bg-white'>
-                    <p className='text-5xl lg:text-6xl font-bold text-indigo-600'>Lorem.</p>
-                    <p className='text-black mt-2 px-2 py-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo adipisci sapiente itaque repudiandae aut, perferendis qui rerum eius? Ab magnam non cum, fugiat quidem ducimus asperiores voluptatibus excepturi, velit incidunt illo. Blanditiis facere accusamus consectetur assumenda, eos dignissimos aliquid nostrum enim. Nisi quia laudantium sed natus neque quae sequi magni deleniti commodi, voluptatibus omnis molestias corrupti assumenda maiores laborum eos a rerum! Ipsam laborum quaerat animi iste dignissimos? Quaerat, totam consectetur aliquid reiciendis porro illo inventore. Odit reprehenderit vel nam!</p>
+                <div className='border py-8 rounded-xl shadow-2xl bg-white justify-center'>
+                    <p className='text-5xl lg:text-6xl font-bold text-indigo-600'>Expertise</p>
+                    <p className='text-black pt-2 '>Being a software engineer also means being a full time student. Heres what I've learned...</p>
+                    <div className='px-2 text-black'> 
+                        <div className=' pb-2 border-b-2 border-b-stone-700'>
+                            <h2 className='font-bold'>Technology that I am proficient with</h2>
+                        </div>
+                        <div className='pt-2'>
+                            <h1>Java, Python, C, Javascript, REACT.js, HTML, CSS, Flask, SQL, and TailwindCSS</h1>
+                        </div>
+
+                        <div className='pt-8 pb-2 border-b-2 border-b-stone-700'>
+                            <h2 className='font-bold'>Technology that I am learning</h2>
+                        </div>
+                        <div className='pt-2'>
+                            <h1>Rust, Typescript, and MongoDB</h1>
+                        </div>
+                    </div>  
                 </div>
             </div>
             
--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -4,14 +4,16 @@ import face from '../assets/help.png'
 export default function Header() {
   return (
   <div class="p-8 md:pt-24 h-full w-full flex items-center justify-center bg-gradient-to-tr from-zinc-800 to-indigo-600 shadow-2xl">
-    <div className='grid md:grid-cols-2 max-w-[1340px] m-auto '>
-      <div className='max-w-[650px] flex flex-col sm:col-span-1 justify-center md:items-start w-full py-8'>
+    <div className='grid md:grid-cols-2 max-w-[1340px] m-auto md:gap-5 '>
+      <div className='max-w-[650px] flex flex-col sm:col-span-1 justify-center md:items-start w-full py-20'>
         <h1 className='text-center md:text-left py-3 text-6xl md:text-7xl font-bold font-outfit text-white'>Phil Ganem</h1>
         <p className='text-3xl md:text-4xl  text-white text-center md:text-left font-outfit'>3rd-Year Software Engineering student at the Rochester Institute of Technology.</p>
       </div>
       <div className='grid max-w-[500px] place-items-center'>
         {/* Fix picture w divs not the same size */}
-        {/* <img className=' w-[300px] h-[350px] md:w-[325px] md:h-[375px] ' src={face} alt="/" /> */}
+        <div className='fill-blue-500 '>
+        <img className=' w-[300px] h-[350px] md:w-[325px] md:h-[375px] ' src={face} alt="/" />
+          </div>
       </div>
     </div>
   </div>
