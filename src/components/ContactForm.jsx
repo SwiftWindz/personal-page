67a01ba38d58bbd51114682287a126cdbb8d9623
Phil Ganem <philipganem@gmail.com>
2022-09-12 14:00:34 -0400

    New contact form works on mobile

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -16,9 +16,9 @@ const [state, handleSubmit] = useForm("xoqrbvgv");
             <p className='text-4xl md:text-6xl font-bold '>Lets get in contact!</p>
             <p className=' mt-2 px-5 py-2 text-3xl md:text-4xl'>Have something you want to tell me? Send me an email below!</p>
           <div className='pt-10'>
-          <form className='bg-zinc-800 rounded-2xl w-[350px] h-[400px] md:w-[700px] md:h-[350px] drop-shadow-xl ' onSubmit={handleSubmit}>
-            <div className='grid grid-cols-1 md:grid-cols-2 md:p-6'>
-              <div className='w-full md:pr-10 text-center md:text-left md:border-r-2 pb-8'>
+          <form className='bg-zinc-800 rounded-2xl w-[350px] h-[450px] md:w-[700px] md:h-[375px] drop-shadow-xl ' onSubmit={handleSubmit}>
+            <div className='grid grid-cols-1 md:grid-cols-2 md:p-6 px-4'>
+              <div className='w-full md:pr-10 text-center md:text-left md:border-r-2 pb-8 pt-8 md:pt-0'>
                 <div className='pb-8'>
                   <p className='text-sm text-gray-400'>Full Name</p>
                   <input className='focus:outline-none border-b-2 focus:border-violet-300 bg-zinc-800 ' id="Name" name="Name" required={true}/>
@@ -28,9 +28,9 @@ const [state, handleSubmit] = useForm("xoqrbvgv");
                   <input className='focus:outline-none border-b-2 focus:border-violet-300 bg-zinc-800 ' id="email" type="email" name="email" required={true}/>
                 </div>
               </div>
-              <div className='border-t-2 md:border-t-0 '>
+              <div className='border-t-2 md:border-t-0 pb-8'>
                 <p className='text-center md:text-left md:pl-4 text-sm text-gray-400 pt-8 md:pt-0'>Message</p>
-                <textarea className=' h-[250px] w-[300px] bg-zinc-800 focus:outline-none resize-none md:pl-1' placeholder='...' required={true} id="message" name="message"/>
+                <textarea className='h-[150px] md:h-[250px] w-[300px] bg-zinc-800 focus:outline-none resize-none md:pl-1' placeholder='Type your message here...' required={true} id="message" name="message"/>
                 <ValidationError prefix="Message" field="message"errors={state.errors}/>
               </div>
             </div>
